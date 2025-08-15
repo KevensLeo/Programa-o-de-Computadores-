45ab5762b89c144cc3ba039b24caf4798a51791f
Keven Silva Leodoro <keven80502@edu.unipar.br>
2025-08-14 22:31:51 -0300

    Add files via upload

---
+++ Java Workspace/Aulas/exemplo_01.java
@@ -0,0 +1,29 @@
+class exemplo_01 {
+
+    public static void main(String[] args){
+
+        System.out.println("Olá Mundo");
+        
+        int idade;
+        idade = 23;
+
+        System.out.println("idade: " + idade);
+
+        String nome;
+        nome = "Keven";
+
+        System.out.println("nome : " + nome);
+
+        boolean status_aluno;
+        status_aluno = true;
+
+        System.out.println("status_aluno : " + status aluno);
+
+        char periodo;
+        periodo = '2';
+        
+        System.out.println("periodo : " + periodo);
+    }
+
+
+}
